83c1a4c55620cce90b2bb7a708e2902b5545146f
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-08-14 16:00:04 -0500

    Habilitando la conexion de cualquier usuario a la api con cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const routerApi = require('./routes')
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send("server de inventario funcionando.")
