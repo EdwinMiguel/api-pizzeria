9106fa6ae7955b62805b6f75b78aabd1dcfa0522
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-08-28 16:34:31 -0500

    Fix: configurando las credenciales de googleapis para subir a github

--- services/order.service.js
+++ services/order.service.js
@@ -1,28 +1,138 @@
 class OrderService {
   constructor () {
     this.orders = [];
+    this.orderOptions = [];
   }
 
-  find () {
-    return this.orders
-  }
+  async find() {
+    const sheetsApi = await this.getGoogleSheetsClient();
+    const spreadsheetId = '1VBk8B9E2uA98Zs3yEqrTl1uFqsRWNVG06LAlqIFazrs';
+    const range = "PRUEBA BD VERCEL";
 
-  async create (data) {
-    const now = new Date()
-    const day = now.getDate()
-    const month = now.getMonth() + 1
-    const year = now.getFullYear()
-    const hour = now.getHours()
-    const minutes = now.getMinutes()
-    const seconds = now.getSeconds()
-    const newOrder = {
-      date: `${day}/${month}/${year} ${hour}:${minutes}:${seconds}`,
-      ...data
+    try {
+      const response = await sheetsApi.spreadsheets.values.get({
+        spreadsheetId,
+        range,
+      });
+      const data = response.data.values;
+      data.shift();
+      const options = await this.getOrderOptions();
+
+      data.forEach((sheetOrder) => {
+        let keyValue = {};
+        options[0].forEach((key, index) => {
+
+          if (sheetOrder[index] !== '') {
+            keyValue[key] = sheetOrder[index];
+          }
+        });
+
+        this.orders.push(keyValue);
+      });
+
+      return this.orders;
+
+    } catch (error) {
+      console.error('Error al obtener los pedidos:', error);
     }
-
-    this.orders.push(newOrder)
-    return newOrder
   }
+
+  async getOrderOptions() {
+    const sheetsApi = await this.getGoogleSheetsClient();
+    const spreadsheetId = '1VBk8B9E2uA98Zs3yEqrTl1uFqsRWNVG06LAlqIFazrs';
+    const range = "PRUEBA BD VERCEL!A1:AP1";
+
+    try {
+      const response = await sheetsApi.spreadsheets.values.get({
+        spreadsheetId,
+        range,
+      });
+      const values = response.data.values;
+
+      return values;
+    } catch (error) {
+      console.error('Error al obtener los pedidos:', error);
+    }
+  }
+
+  async appendOrder(orderData) {
+    const sheetsApi = await this.getGoogleSheetsClient();
+
+    const spreadsheetId = '1VBk8B9E2uA98Zs3yEqrTl1uFqsRWNVG06LAlqIFazrs';
+    const range = 'PRUEBA BD VERCEL';
+    const valueInputOption = 'USER_ENTERED';
+
+    const formData = Object.entries(orderData);
+    const data = await this.getData();
+
+    const order = [];
+    const options = data.data.values[0];
+
+    options.forEach(option => {
+      const contain = formData.find(element => element.includes(option));
+      if (contain === undefined) {
+        order.push('');
+      } else if (contain[0] === option) {
+        order.push(contain[1]);
+      }
+    });
+
+    console.log('order', order)
+
+    const resource = {
+      values: [
+        order,
+      ]
+    };
+
+    try {
+      const response = await sheetsApi.spreadsheets.values.append({
+        spreadsheetId,
+        range,
+        valueInputOption,
+        resource,
+      });
+      console.log(`${response.data.updates.updatedCells} celdas actualizadas.`);
+    } catch (error) {
+      console.error('Error al escribir en Google Sheets:', error);
+    }
+  }
+
+  async getData() {
+    const sheetsApi = await this.getGoogleSheetsClient();
+    const spreadsheetId = '1VBk8B9E2uA98Zs3yEqrTl1uFqsRWNVG06LAlqIFazrs';
+    const range = "PRUEBA BD VERCEL!A:AP";
+
+    try {
+      const response = await sheetsApi.spreadsheets.values.get({
+        spreadsheetId,
+        range,
+      });
+      return response;
+    } catch (error) {
+      console.error('Error al obtener los pedidos:', error);
+    }
+  }
+
+  async getGoogleSheetsClient() {
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      },
+      project_id: process.env.GOOGLE_PROJECT_ID,
+      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+
+    const client = await auth.getClient();
+    const sheetsApi = google.sheets({
+      version: 'v4',
+      auth: client
+    });
+
+    return sheetsApi;
+  }
+
 
 }
 
