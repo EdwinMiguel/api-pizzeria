ef56c356f7776cb1757ebc2adf97e52428d185a8
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-09-27 16:54:15 -0500

    Refactor: cambio en save() product

--- services/product.service.js
+++ services/product.service.js
@@ -64,65 +64,32 @@ class ProductsService {
       const productsData = sheetData.data.values;
       const headers = productsData[0].map(header => header.trim());
 
-      let range;
-      let values;
-      // {
-      //   'product-name': 'ATÚN',
-      //   quantity: '5',
-      //   unit: 'libras',
-      //   'arrival-date': '2024-09-27'
-      // }
+      const row = headers.map(() => '');
 
-      productsData.forEach((row, index) => {
-        if (row.includes(formData['product-name'])) {
-          formData.unit.toLowerCase();
-          if (formData.unit === 'libras') {
-            const rowIndex = createNewValues('LIBRAS STOCK', row, index);
-            range = getCell('LIBRAS STOCK', rowIndex);
-          } else if (formData.unit === 'unidades') {
-            const rowIndex = createNewValues('UNIDADES STOCK', row, index);
-            range = getCell('UNIDADES STOCK', rowIndex);
-          } else if (formData.unit === 'frascos') {
-            const rowIndex = createNewValues('FRASCOS STOCK', row, index);
-            range = getCell('FRASCOS STOCK', rowIndex);
-          } else if (formData.unit === 'bloques') {
-            const rowIndex = createNewValues('BLOQUES STOCK', row, index);
-            range = getCell('BLOQUES STOCK', rowIndex);
-          } else if (formData.unit === 'bandejas') {
-            const rowIndex = createNewValues('BANDEJAS STOCK', row, index);
-            range = getCell('BANDEJAS STOCK', rowIndex);
-          }
+      headers.forEach((header, headerIndex) => {
+        if (header === 'PRODUCTO') {
+          row[headerIndex] = formData['product-name'];
+        } else if (header === 'FECHA INGRESO') {
+          row[headerIndex] = formData['arrival-date'];
+        } else if (header === 'TIPO MEDIDA') {
+          row[headerIndex] = formData.unit;
+        } else if (header === 'CANTIDAD INGRESO') {
+          const quantityToNumber = Number(formData.quantity);
+          row[headerIndex] = quantityToNumber;
         }
       });
 
-      function createNewValues(headerName, row, currentIndex) {
-        const headerIndex = headers.indexOf(headerName);
-            console.log(headerIndex);
-            const newQuantity = Number(formData.quantity) + Number(row[headerIndex]);
-            values = {
-              values: [[newQuantity]]
-            }
-            const rowIndex = currentIndex + 1;
-            return rowIndex;
+      const values = {
+        values: [row]
       }
 
-      function getCell(headerName, rowIndex) {
-        const columnIndex = headers.indexOf(headerName);
-        const columnLetter = String.fromCharCode(65 + columnIndex);
-        const range = `${sheetName}!${columnLetter}${rowIndex}`;
-        return range;
-      }
-
-      async function updateCell(cellRange, value) {
-        const response = await sheetsApi.spreadsheets.values.update({
+        const response = await sheetsApi.spreadsheets.values.append({
           spreadsheetId: spreadsheetId,
-          range: cellRange,
+          range: sheetName,
           valueInputOption: 'USER_ENTERED',  // 'RAW' para valores sin formato, 'USER_ENTERED' para valores como si los escribiera un usuario
-          resource: value
+          resource: values
         });
         return response;
-      }
-      const response = await updateCell(range, values);
     } catch (error) {
       console.log(error);
     }
