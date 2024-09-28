a6a139d246fcc05f9392e9f909d2aa4f0427cab1
Edwin Miguel <edwinmiguelorellana@gmail.com>
2024-09-28 00:51:04 -0500

    Refactor: guardar stock con save()

--- services/product.service.js
+++ services/product.service.js
@@ -7,44 +7,61 @@ class ProductsService {
   }
 
   async find () {
-    const spreadsheetId = '1VBk8B9E2uA98Zs3yEqrTl1uFqsRWNVG06LAlqIFazrs';
-    const range = "LISTA PRECIOS";
 
     try {
-      const auth = new google.auth.GoogleAuth({
-        credentials: {
-          private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-          client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        },
-        project_id: process.env.GOOGLE_PROJECT_ID,
-        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+      const sheetsApi = await getGoogleSheetsClient();
+      const spreadsheetId = '1VBk8B9E2uA98Zs3yEqrTl1uFqsRWNVG06LAlqIFazrs';
+      const range = "STOCK";
+      const productsSheet = await sheetsApi.spreadsheets.values.get({
+        spreadsheetId: spreadsheetId,
+        range: range,
       });
+      const productsSheetData = productsSheet.data.values;
+      productsSheetData.shift();
 
-      const client = await auth.getClient();
-      const sheetsApi = google.sheets({
-        version: 'v4',
-        auth: client
-      });
+      /*
+      {
+        product-name: ,
+        stock: ,
+        valor-total: ,
+      }
+      */
 
-      const response = await sheetsApi.spreadsheets.values.get({
-        spreadsheetId,
-        range,
-      });
-      const data = response.data.values;
-      data.shift();
-      const productsList = [];
-      const products = data;
-      products.forEach(item => {
-        const product = {
-          id: item[0],
-          name: item[1],
-          price: item[2]
-        }
-        productsList.push(product);
-      })
-      this.products = productsList;
 
-      return this.products;
+      // const auth = new google.auth.GoogleAuth({
+      //   credentials: {
+      //     private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      //     client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      //   },
+      //   project_id: process.env.GOOGLE_PROJECT_ID,
+      //   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+      // });
+
+      // const client = await auth.getClient();
+      // const sheetsApi = google.sheets({
+      //   version: 'v4',
+      //   auth: client
+      // });
+
+      // const response = await sheetsApi.spreadsheets.values.get({
+      //   spreadsheetId,
+      //   range,
+      // });
+      // const data = response.data.values;
+      // data.shift();
+      // const productsList = [];
+      // const products = data;
+      // products.forEach(item => {
+      //   const product = {
+      //     id: item[0],
+      //     name: item[1],
+      //     price: item[2]
+      //   }
+      //   productsList.push(product);
+      // })
+      // this.products = productsList;
+
+      // return this.products;
     } catch (error) {
       console.error(error);
     }
@@ -55,41 +72,54 @@ class ProductsService {
     try {
       const sheetsApi = await getGoogleSheetsClient();
       const spreadsheetId = '1VBk8B9E2uA98Zs3yEqrTl1uFqsRWNVG06LAlqIFazrs';
-      const sheetName = "STOCK";
+      const sheetName = "PRODUCTO";
       const sheetData = await sheetsApi.spreadsheets.values.get({
         spreadsheetId: spreadsheetId,
         range: sheetName,
       });
 
-      const productsData = sheetData.data.values;
-      const headers = productsData[0].map(header => header.trim());
-
-      const row = headers.map(() => '');
-
-      headers.forEach((header, headerIndex) => {
-        if (header === 'PRODUCTO') {
-          row[headerIndex] = formData['product-name'];
-        } else if (header === 'FECHA INGRESO') {
-          row[headerIndex] = formData['arrival-date'];
-        } else if (header === 'TIPO MEDIDA') {
-          row[headerIndex] = formData.unit;
-        } else if (header === 'CANTIDAD INGRESO') {
-          const quantityToNumber = Number(formData.quantity);
-          row[headerIndex] = quantityToNumber;
-        }
+      const productsData = sheetData.data.values.map(row => {
+          return row.map(value => value.trim());
       });
+      const headers = productsData[0];
 
-      const values = {
-        values: [row]
+      function getCell(headerName, rowIndex) {
+        const columnIndex = headers.indexOf(headerName);
+        const columnLetter = String.fromCharCode(65 + columnIndex);
+        const range = `${sheetName}!${columnLetter}${rowIndex}`;
+        return range;
       }
 
-        const response = await sheetsApi.spreadsheets.values.append({
+      async function postValue(cellRange, resource) {
+        const response = await sheetsApi.spreadsheets.values.update({
           spreadsheetId: spreadsheetId,
-          range: sheetName,
+          range: cellRange,
           valueInputOption: 'USER_ENTERED',  // 'RAW' para valores sin formato, 'USER_ENTERED' para valores como si los escribiera un usuario
-          resource: values
+          resource: resource
         });
         return response;
+      }
+
+      let range;
+      const stockValue = {
+        values: []
+      }
+
+      productsData.forEach((row, rowIndex)=> {
+        if (row.includes(formData['product-name'])) {
+          if (row.length < headers.length) {
+            for (let index = row.length; index < headers.length; index++) {
+              row.push('');
+            }
+          }
+          range = getCell('stock', rowIndex + 1);
+          const headerIndex = headers.indexOf('stock');
+          const quantity = Number(formData.quantity) + Number(row[headerIndex]);
+          stockValue.values.push([quantity]);
+        }
+      });
+      const response = await postValue(range, stockValue);
+      return response;
     } catch (error) {
       console.log(error);
     }
